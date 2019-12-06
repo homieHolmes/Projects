3a197dfda60231c10d04fa2cb07e0bfa67067dc2
Fransis-tea001 <AtipBoonyong2560@gmail.com>
2019-12-07 02:41:08 +0700

    Add Student Struct

--- FileAccess.c
+++ FileAccess.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+//Student Struct
+typedef struct{
+    char id[10], name[30], tel[11], gmail[30];
+    int age;
+}Student;
 
 int main(){
 
