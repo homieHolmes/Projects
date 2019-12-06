6ac547017666d72d95412373dbda934ffb1b7350
homieHolmes <chu.zahil@gmail.com>
2019-12-07 02:39:30 +0700

    update FileAccess

--- FileAccess.c
+++ FileAccess.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
 
+// addfunction();
+// locatefunction();
+//name id phonenumber email
 int main(){
 
-    printf("Test from GithubDesktop for pull request");
+    char choose;
+    printf("Do you want to add more or locate (A / L) : ");
+    scanf("%c",&choose);
+
+    if(strcmp(choose, "A") == 0)
+        printf("Adding");
+        //Addfunction();
+        else if (strcmp(choose, "L") == 0 )
+        printf("Locating");
+        //Locatefunction();
+             
     return 0;
 }
