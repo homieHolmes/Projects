53050ebe38f9ecb89d128279ce3e0d43593164b8
homieHolmes <chu.zahil@gmail.com>
2019-12-07 03:12:34 +0700

    Update FileAccess.c

--- FileAccess.c
+++ FileAccess.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-#include<conio.h>
 #include<stdlib.h>
 
-//Student Struct
+//Student Struct 
 typedef struct{
     char id[10], name[30], tel[11], gmail[30];
     int age;
 }Student;
 
 
-// addfunction();
-// locatefunction();
-//name id phonenumber email
+
 int main(){
 
     char choose;
+    do{
     printf("Do you want to add more or locate (A / L) : ");
-    scanf("%c",&choose);
+    scanf("%c",choose);
 
-    if(choose == 'A')
-        printf("Adding");
-        //Addfunction();
+    if(choose != 'A' && choose != 'L')
+        printf("Please choose only A (for Adding) and L (for Locating).");
+
+    }while(choose != 'A' || choose != 'L');
+
+system("cls");
+
+    if(choose == 'A'){
+            //Addfunction();
         else if (choose == 'L')
-        printf("Locating");
-        //Locatefunction();
-             
+            //Locatefunction();
+    
+
+
+
+
     return 0;
 }
